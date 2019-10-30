5284f043ea5488d81d58dbc488f24baf203db5a9
andrelsfrazao@gmail.com <andrelsfrazao@gmail.com>
2019-10-29 21:54:32 -0300

    Implementação dos Modelos de Classes e Implementação das Classes de Requisição HTTP utilizando tanto Newtonsoft como System.Text.Json

---
+++ MovieDBApp/MovieDBApp/MovieDBApp.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 536ee1a28a5e7ce9bdef37103c7e1ee8939e2e61 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1282
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 335885
+	.include	"typemap.jm.inc"
---
+++ MovieDBApp/MovieDBApp/MovieDBApp/Services/IMovieDBService.cs
@@ -0,0 +1,17 @@
+﻿using MovieDBApp.Models;
+using MovieDBApp.NovaPasta2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDBApp.NovaPasta1
+{
+    interface IMovieDBService
+    {
+        
+        Task<SearchResponse<Movie>> GetUpcomingMoviesAsync(int pageNumber = 1, string language = "en");
+        Task<Movie> FindByIdAsync(int movieId, string language = "en");
+    }
+}
+
